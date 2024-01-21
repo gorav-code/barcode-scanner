6de3b97defe4dbb5cef1aa2f7f8a97a77ff4c9ef
gorav-code <gorav.code@outlook.com>
2024-01-21 08:02:51 +0530

    initial commit

---
+++ barcode-scanner/barcode-scanner/barcode_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcode_scannerApp.swift
+//  barcode-scanner
+//
+//  Created by Gaurav Kundalwal on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct barcode_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
